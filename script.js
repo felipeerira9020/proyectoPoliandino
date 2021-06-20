9a2b13489314fa5ed6c65b6b7f96d8a8a46d19cb
felipeerira9020 <felipe.erira9020@gmail.com>
2021-06-20 10:40:09 -0500

    Js actualizacion contador

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+// variables del contador de certificados
 let contador = document.querySelector(".numero1");
 let contador1 = document.querySelector(".numero2");
-let contador2= document.querySelector(".numero3");
+let contador2 = document.querySelector(".numero3");
 let contador3 = document.querySelector(".numero4");
 let sectionContador = document.querySelector(".contador")
 let num = 0;
@@ -14,41 +14,39 @@ let num3 = 0;
 contador3.innerHTML = num3;
 
 sectionContador.addEventListener("mouseover", function () {
-    let timer = setInterval(function () {  
+    let timer = setInterval(function () {
         if (num > 864) {
-            clearInterval(timer)       
-        }else
-        num++
+            clearInterval(timer)
+        } else
+            num++
         contador.innerHTML = num;
-    },0.6)    
+    }, 0.6)
 })
-
 sectionContador.addEventListener("mouseover", function () {
-    let timer = setInterval(function () {  
+    let timer = setInterval(function () {
         if (num1 > 468) {
-            clearInterval(timer)       
-        }else
-        num1++
+            clearInterval(timer)
+        } else
+            num1++
         contador1.innerHTML = num1;
-    },10)    
+    }, 10)
 })
-
 sectionContador.addEventListener("mouseover", function () {
-    let timer = setInterval(function () {  
+    let timer = setInterval(function () {
         if (num2 > 1468) {
-            clearInterval(timer)       
-        }else
-        num2++
+            clearInterval(timer)
+        } else
+            num2++
         contador2.innerHTML = num2;
-    },0.00001)    
+    }, 0.00001)
 })
 
 sectionContador.addEventListener("mouseover", function () {
-    let timer = setInterval(function () {  
+    let timer = setInterval(function () {
         if (num3 > 32) {
-            clearInterval(timer)       
-        }else
-        num3++
+            clearInterval(timer)
+        } else
+            num3++
         contador3.innerHTML = num3;
-    },100)    
+    }, 100)
 })
